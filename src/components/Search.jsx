ba8d9be5da201bc268c52b85407bc3fe42610c54
BiaTrixie <bmoura276@gmail.com>
2024-05-27 19:46:31 -0300

    corrigindo escrever blog

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -32,7 +32,7 @@ const Search = () => {
           type='text'
           id='simple-search'
           className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 py-2 pl-10 text-sm text-gray-900 shadow-xl focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600  dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:py-3'
-          placeholder='SBuscar por artigo'
+          placeholder='Buscar por artigos'
           required
         />
       </div>
